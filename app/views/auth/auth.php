1b5b89fee48b26ea9c7abece099e27699402ce42
Jérémy Pingeon <jeremy@justauth.me>
2019-04-27 22:18:32 +0200

    Auth design updated

--- app/views/auth/auth.php
+++ app/views/auth/auth.php
@@ -1,49 +1,54 @@
-<h1>JustAuth.Me</h1>
+	<div class="container">
+		<div class="login">
+			<img src="" alt="" />
+			<h1>You're about to log into alpha.justauth.me/demo</h1>
+			<p>Just scan the following QR-Code with your JustAuth.Me mobile App</p>
+			<img class="qrcode" src="<?= $qr_code ?>" alt="login qr code" />
+			<form style="display:none" id="submit_form"></form>
+		</div>
+		
+		<div class="footer">
+			<img src="assets/img/logo.png" alt="JustAuthMe Logo" />
+		</div>
+	</div>
+	<script type="text/javascript">
+		var form = document.getElementById('submit_form');
+		form.method = 'post';
+		form.action = '<?= $auth->getCallbackUrl() ?>';
+		var conn = new WebSocket('<?= WEBSOCKET_SOCKET_REMOTE ?>');
+		conn.onopen = function(e) {
+			console.log("Connection established!");
+			var msg = {
+				'type': 'await',
+				'auth_id': '<?= $auth->getToken() ?>'
+			};
+			console.log('Message sent', msg);
+			conn.send(JSON.stringify(msg));
+		};
 
-<h3>You're about to log into <?= $client->getDomain() ?></h3>
+		conn.onmessage = function(e) {
+			var data = JSON.parse(e.data);
+			console.log('Message received', data);
+			if (data.type && data.type === 'data') {
+				for (var i in data.data) {
+					var input = document.createElement('input');
+					input.type = 'text';
+					input.name = i;
+					input.value = data.data[i];
+					form.appendChild(input);
+				}
 
-<p>Just scan the following QR-Code with your JustAuth.Me mobile App</p>
+				console.log(form);
+				form.submit();
+				conn.close();
+			}
+		};
 
-<img src="<?= $qr_code ?>" alt="login qr code" />
-<form style="display:none" id="submit_form"></form>
-<script type="text/javascript">
-    var form = document.getElementById('submit_form');
-    form.method = 'post';
-    form.action = '<?= $auth->getCallbackUrl() ?>';
-    var conn = new WebSocket('<?= WEBSOCKET_SOCKET_REMOTE ?>');
-    conn.onopen = function(e) {
-        console.log("Connection established!");
-        var msg = {
-            'type': 'await',
-            'auth_id': '<?= $auth->getToken() ?>'
-        };
-        console.log('Message sent', msg);
-        conn.send(JSON.stringify(msg));
-    };
+		conn.onerror = function(e) {
+			console.log(e);
+		};
 
-    conn.onmessage = function(e) {
-        var data = JSON.parse(e.data);
-        console.log('Message received', data);
-        if (data.type && data.type === 'data') {
-            for (var i in data.data) {
-                var input = document.createElement('input');
-                input.type = 'text';
-                input.name = i;
-                input.value = data.data[i];
-                form.appendChild(input);
-            }
-
-            console.log(form);
-            form.submit();
-            conn.close();
-        }
-    };
-
-    conn.onerror = function(e) {
-        console.log(e);
-    };
-
-    conn.onclose = function(e) {
-        console.log('Connection closed');
-    };
-</script>
+		conn.onclose = function(e) {
+			console.log('Connection closed');
+		};
+	</script>
--- app/views/mainView.php
+++ app/views/mainView.php
@@ -3,7 +3,6 @@
 	<head>
 		<title><?php echo (isset($TITLE)) ? $TITLE.' - ' : ''; echo NAME; ?></title>
 		<meta charset="utf-8" />
-		<link type="text/css" rel="stylesheet" href="<?php echo CSS.'bootstrap.min.css'; ?>" media="screen" />
 		<link type="text/css" rel="stylesheet" href="<?php echo CSS.'style.css'; ?>" media="screen" />
 		<meta name="viewport" content="user-scalable=no, width=device-width, initial-scale=1.0" />
 		<meta name="format-detection" content="telephone=no" />
@@ -13,7 +12,6 @@
 		<?php require_once @$appView; ?>
 		
 		<script type="text/javascript" src="<?php echo JS.'jquery.min.js'; ?>"></script>
-		<script type="text/javascript" src="<?php echo JS.'bootstrap.min.js'; ?>"></script>
 		<script type="text/javascript" src="<?php echo JS.'script.js'; ?>"></script>
 	</body>
 </html>
