5c1042bfd78ef980fb13333086dec1f0db98e687
PHPeter <phpeter@phpeter.fr>
2019-05-01 13:10:37 +0200

    Update example config

--- config.example.php
+++ config.example.php
@@ -4,8 +4,24 @@
  */
 
 const PROD_ENV = false;
+const PROD_HOST = 'localhost';
 
-const DB_HOST = 'localhost';
-const DB_NAME = '';
-const DB_USER = 'root';
-const DB_PASS = '';
+const DATA_TRANSFERT_KEY = 'some random shit';
+const DB_HOST = 'do host';
+const DB_NAME = 'justauthme';
+const DB_USER = 'db user';
+const DB_PASS = 'db pass';
+
+const REDIS_HOST = '127.0.0.1';
+const REDIS_PORT = 6379;
+const REDIS_PASS = 'redis pass (must be loooooooooooooooong';
+
+const WEBSOCKET_HOST = 'localhost';
+const WEBSOCKET_PORT = 1337;
+const WEBSOCKET_PATH = '';
+const WEBSOCKET_SOCKET_LOCAL = 'ws://' . WEBSOCKET_HOST . ':' . WEBSOCKET_PORT;
+const WEBSOCKET_SOCKET_REMOTE = 'ws://' . PROD_HOST . (
+    WEBSOCKET_PATH !== '' ?
+        WEBSOCKET_PATH :
+        ':' . WEBSOCKET_PORT
+    );
