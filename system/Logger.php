b65ca7dbe15c64eba00037c2cc5f51bbdb91c6ac
PHPeter <phpeter@phpeter.fr>
2019-05-23 22:07:35 +0200

    Debug logger

--- system/Logger.php
+++ system/Logger.php
@@ -11,17 +11,17 @@ class Logger {
     const LOG_FILE_WARNING = ROOT . 'log/warning.log';
     const LOG_FILE_ERROR = ROOT . 'log/error.log';
 
-    private static function log($message, $filename = self::LOG_FILE_INFO) {
+    private static function log($message, $filename = self::LOG_FILE_INFO, $backtrace = null) {
         if (in_array($filename, [
             self::LOG_FILE_INFO,
             self::LOG_FILE_WARNING,
             self::LOG_FILE_ERROR
         ])) {
-            $bt = debug_backtrace();
-            $caller = array_shift($bt);
+            $backtrace = $backtrace === null ? debug_backtrace() : $backtrace;
+            $caller = array_shift($backtrace);
             $data = '------------------------------------------' . "\n" .
                 '[' . date('d/m/Y H:i:s') . '][' . $_SERVER['REMOTE_ADDR'] . ']' . "\n" .
-                '[in ' . $caller['file'] . ' line' . $caller['line'] . ']' . "\n" .
+                '[in ' . $caller['file'] . ' line ' . $caller['line'] . ']' . "\n" .
                 $message . "\n\n";
             file_put_contents($filename, $data, FILE_APPEND);
         } else {
@@ -30,14 +30,14 @@ class Logger {
     }
 
     public static function logInfo($message) {
-        self::log($message, self::LOG_FILE_INFO);
+        self::log($message, self::LOG_FILE_INFO, debug_backtrace());
     }
 
     public static function logWarning($message) {
-        self::log($message, self::LOG_FILE_WARNING);
+        self::log($message, self::LOG_FILE_WARNING, debug_backtrace());
     }
 
     public static function logError($message) {
-        self::log($message, self::LOG_FILE_ERROR);
+        self::log($message, self::LOG_FILE_ERROR, debug_backtrace());
     }
 }
