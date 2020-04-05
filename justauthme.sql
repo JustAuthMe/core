e91f022ea63ecb3d758a8540888f3c91b0a5bd85
Jérémy Pingeon <pingeon.jeremy@icloud.com>
2020-04-05 18:56:00 +0200

    SQL updated

--- justauthme.sql
+++ justauthme.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.5
+-- version 4.9.5
 -- https://www.phpmyadmin.net/
 --
--- Hôte : mamp_mysql:3306
--- Généré le :  sam. 04 jan. 2020 à 20:55
--- Version du serveur :  8.0.18
--- Version de PHP :  7.2.14
+-- Hôte : localhost
+-- Généré le : Dim 05 avr. 2020 à 18:54
+-- Version du serveur :  5.7.29-0ubuntu0.16.04.1
+-- Version de PHP : 7.4.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -19,10 +19,8 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de données :  `justauthme`
+-- Base de données : `jam_core`
 --
-CREATE DATABASE IF NOT EXISTS `justauthme` DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;
-USE `justauthme`;
 
 -- --------------------------------------------------------
 
@@ -30,23 +28,33 @@ USE `justauthme`;
 -- Structure de la table `client_app`
 --
 
-DROP TABLE IF EXISTS `client_app`;
-CREATE TABLE IF NOT EXISTS `client_app` (
-`id` int(11) NOT NULL AUTO_INCREMENT,
-`domain` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
-`app_id` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
-`name` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
-`logo` varchar(1023) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
-`redirect_url` varchar(1023) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
-`data` text CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
-`public_key` text CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
-`secret` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
-`hash_key` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-PRIMARY KEY (`id`),
-UNIQUE KEY `domain` (`domain`),
-UNIQUE KEY `app_id` (`app_id`),
-UNIQUE KEY `name` (`name`),
-UNIQUE KEY `redirect_url` (`redirect_url`)
+CREATE TABLE `client_app` (
+  `id` int(11) NOT NULL,
+  `domain` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `app_id` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `logo` varchar(1023) COLLATE utf8_unicode_ci NOT NULL,
+  `redirect_url` varchar(1023) COLLATE utf8_unicode_ci NOT NULL,
+  `data` text COLLATE utf8_unicode_ci NOT NULL,
+  `public_key` text COLLATE utf8_unicode_ci NOT NULL,
+  `secret` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `hash_key` varchar(255) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `uniqid_update`
+--
+
+CREATE TABLE `uniqid_update` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `old_uniqid` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `new_uniqid` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `ip_address` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `timestamp` int(11) NOT NULL,
+  `active` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- --------------------------------------------------------
@@ -55,17 +63,14 @@ UNIQUE KEY `redirect_url` (`redirect_url`)
 -- Structure de la table `user`
 --
 
-DROP TABLE IF EXISTS `user`;
-CREATE TABLE IF NOT EXISTS `user` (
-`id` int(11) NOT NULL AUTO_INCREMENT,
-`username` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
-`uniqid` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-`timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-`ip_address` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
-`public_key` text CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
-`active` tinyint(1) NOT NULL DEFAULT '0',
-PRIMARY KEY (`id`),
-UNIQUE KEY `uniqid` (`uniqid`)
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `username` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `uniqid` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `ip_address` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `public_key` text COLLATE utf8_unicode_ci NOT NULL,
+  `active` tinyint(1) NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- --------------------------------------------------------
@@ -74,17 +79,13 @@ UNIQUE KEY `uniqid` (`uniqid`)
 -- Structure de la table `user_auth`
 --
 
-DROP TABLE IF EXISTS `user_auth`;
-CREATE TABLE IF NOT EXISTS `user_auth` (
-`id` int(11) NOT NULL AUTO_INCREMENT,
-`token` varchar(64) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
-`client_app_id` int(11) DEFAULT NULL,
-`callback_url` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
-`data` text CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
-`timestamp` bigint(20) NOT NULL,
-`ip_address` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
-PRIMARY KEY (`id`),
-KEY `client_app_id` (`client_app_id`)
+CREATE TABLE `user_auth` (
+  `id` int(11) NOT NULL,
+  `token` varchar(64) COLLATE utf8_unicode_ci NOT NULL,
+  `client_app_id` int(11) DEFAULT NULL,
+  `callback_url` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `timestamp` bigint(20) NOT NULL,
+  `ip_address` varchar(255) COLLATE utf8_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- --------------------------------------------------------
@@ -93,14 +94,88 @@ KEY `client_app_id` (`client_app_id`)
 -- Structure de la table `user_login`
 --
 
-DROP TABLE IF EXISTS `user_login`;
-CREATE TABLE IF NOT EXISTS `user_login` (
-`id` int(11) NOT NULL AUTO_INCREMENT,
-`hash` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-`salt` varchar(31) COLLATE utf8_unicode_ci NOT NULL,
-PRIMARY KEY (`id`),
-UNIQUE KEY `hash` (`hash`)
+CREATE TABLE `user_login` (
+  `id` int(11) NOT NULL,
+  `hash` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `salt` varchar(31) COLLATE utf8_unicode_ci NOT NULL,
+  `active` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `client_app`
+--
+ALTER TABLE `client_app`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `domain` (`domain`),
+  ADD UNIQUE KEY `app_id` (`app_id`);
+
+--
+-- Index pour la table `uniqid_update`
+--
+ALTER TABLE `uniqid_update`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- Index pour la table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `uniqid` (`uniqid`),
+  ADD UNIQUE KEY `username` (`username`);
+
+--
+-- Index pour la table `user_auth`
+--
+ALTER TABLE `user_auth`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `token` (`token`),
+  ADD KEY `client_app_id` (`client_app_id`);
+
+--
+-- Index pour la table `user_login`
+--
+ALTER TABLE `user_login`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `hash` (`hash`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `client_app`
+--
+ALTER TABLE `client_app`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `uniqid_update`
+--
+ALTER TABLE `uniqid_update`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `user_auth`
+--
+ALTER TABLE `user_auth`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `user_login`
+--
+ALTER TABLE `user_login`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- Contraintes pour les tables déchargées
@@ -110,7 +185,8 @@ UNIQUE KEY `hash` (`hash`)
 -- Contraintes pour la table `user_auth`
 --
 ALTER TABLE `user_auth`
-ADD CONSTRAINT `user_auth_ibfk_1` FOREIGN KEY (`client_app_id`) REFERENCES `client_app` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `user_auth_ibfk_1` FOREIGN KEY (`client_app_id`) REFERENCES `client_app` (`id`);
+COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
