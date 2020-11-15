43f7ce58f04a4e5770a99adddb8302f8721f9221
PHPeter <phpeter@phpeter.fr>
2020-11-15 14:44:22 +0100

    Debug get by url

--- app/controllers/api/client_app.php
+++ app/controllers/api/client_app.php
@@ -86,10 +86,13 @@ switch ($_SERVER['REQUEST_METHOD']) {
 
                 Controller::http400BadRequest();
                 Controller::renderApiError('Wrong list format');
-            } elseif (isset($_GET['url']) && Persist::exists('ClientApp', 'url', $_GET['url'])) {
-                $client_app = Persist::readBy('ClientApp', 'url', $_GET['url']);
-                Data::get()->add('client_app', $client_app);
-                Controller::renderApiSuccess();
+            } elseif (isset($_GET['url'])) {
+                $url = trim($_GET['url'], '/');
+                if (Persist::exists('ClientApp', 'url', $url)) {
+                    $client_app = Persist::readBy('ClientApp', 'url', $url);
+                    Data::get()->add('client_app', $client_app);
+                    Controller::renderApiSuccess();
+                }
             }
 
             Controller::http404NotFound();
