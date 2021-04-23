79a55cc5fe30f7f3b826450e9b80fb81fd0cf0f1
phpitou <phpeter@phpeter.fr>
2021-04-23 16:24:16 +0200

    Exclude team members from triggering pixel + add rate limit

--- app/controllers/pixel.php
+++ app/controllers/pixel.php
@@ -1,9 +1,16 @@
 <?php
 const PIXEL_SIGNING_KEY = 'aMJn6AbtUc855uL7BMdPwuMhRBU4WjuqNTtrHo58lGA2jz3jazn1Z/Y5Y+Pt';
+const PIXEL_CACHE_PREFIX = 'pixel_';
+const PIXEL_CACHE_TTL = 86400; // 24 hours
 
-if (isset($_GET['from'], $_GET['to'], $_GET['key'])) {
+$not_allowed_ips = ['45.155.168.240', '185.216.24.254'];
+if (!in_array($_SERVER['REMOTE_ADDR'], $not_allowed_ips) && isset($_GET['from'], $_GET['to'], $_GET['key'])) {
     $calculated_signature = hash_hmac('sha256', $_GET['from'] . '>' . $_GET['to'], PIXEL_SIGNING_KEY);
-    if ($calculated_signature === $_GET['key'] && preg_match("#\@justauth\.me$#", $_GET['from'])) {
+
+    $redis = new \PHPeter\Redis();
+    $cache_key = PIXEL_CACHE_PREFIX . $calculated_signature;
+    $cached = $redis->get($cache_key);
+    if ($cached === false && $calculated_signature === $_GET['key'] && preg_match("#\@justauth\.me$#", $_GET['from'])) {
         $message = $_GET['to'] . ' opened an e-mail ' . date('Y-m-d \a\t H:i:s');
         $mailer = new Mailer();
         $mailer->queueMail(
@@ -12,6 +19,7 @@ if (isset($_GET['from'], $_GET['to'], $_GET['key'])) {
             'mail/blank',
             ['body' => $message]
         );
+        $redis->set($cache_key, 1, PIXEL_CACHE_TTL);
     }
 }
 
