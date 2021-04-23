744fb65a687f27dc4e9124be2ab858ca1cb035df
phpitou <phpeter@phpeter.fr>
2021-04-23 16:33:51 +0200

    Add time parameter

--- app/controllers/pixel.php
+++ app/controllers/pixel.php
@@ -4,14 +4,14 @@ const PIXEL_CACHE_PREFIX = 'pixel_';
 const PIXEL_CACHE_TTL = 86400; // 24 hours
 
 $not_allowed_ips = ['45.155.168.240', '185.216.24.254'];
-if (!in_array($_SERVER['REMOTE_ADDR'], $not_allowed_ips) && isset($_GET['from'], $_GET['to'], $_GET['key'])) {
-    $calculated_signature = hash_hmac('sha256', $_GET['from'] . '>' . $_GET['to'], PIXEL_SIGNING_KEY);
+if (!in_array($_SERVER['REMOTE_ADDR'], $not_allowed_ips) && isset($_GET['from'], $_GET['to'], $_GET['time'], $_GET['key'])) {
+    $calculated_signature = hash_hmac('sha256', $_GET['from'] . '.' . $_GET['to'] . '.' . $_GET['time'], PIXEL_SIGNING_KEY);
 
     $redis = new \PHPeter\Redis();
     $cache_key = PIXEL_CACHE_PREFIX . $calculated_signature;
     $cached = $redis->get($cache_key);
     if ($cached === false && $calculated_signature === $_GET['key'] && preg_match("#\@justauth\.me$#", $_GET['from'])) {
-        $message = $_GET['to'] . ' opened an e-mail ' . date('Y-m-d \a\t H:i:s');
+        $message = $_GET['to'] . ' opened an e-mail ' . date('Y-m-d \a\t H:i:s', Utils::time());
         $mailer = new Mailer();
         $mailer->queueMail(
             $_GET['from'],
