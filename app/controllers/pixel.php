510493faf51662749151109391c87ca5b136cd7d
PHPeter <phpeter@phpeter.fr>
2021-04-24 00:09:15 +0200

    Add IP address to message and cache key + reduce cache ttl to 6 hours

--- app/controllers/pixel.php
+++ app/controllers/pixel.php
@@ -1,7 +1,7 @@
 <?php
 const PIXEL_SIGNING_KEY = 'aMJn6AbtUc855uL7BMdPwuMhRBU4WjuqNTtrHo58lGA2jz3jazn1Z/Y5Y+Pt';
 const PIXEL_CACHE_PREFIX = 'pixel_';
-const PIXEL_CACHE_TTL = 86400; // 24 hours
+const PIXEL_CACHE_TTL = 21600; // 6 hours
 
 $base64 = 'iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mP8z8DwHwAFBQIAX8jx0gAAAABJRU5ErkJggg==';
 $not_allowed_ips = ['45.155.168.240', '185.216.24.254'];
@@ -10,10 +10,10 @@ if (!in_array($_SERVER['REMOTE_ADDR'], $not_allowed_ips) && isset($_GET['from'],
     $calculated_signature = hash_hmac('sha256', $_GET['from'] . '.' . $_GET['to'] . '.' . $_GET['time'], PIXEL_SIGNING_KEY);
 
     $redis = new \PHPeter\Redis();
-    $cache_key = PIXEL_CACHE_PREFIX . $calculated_signature;
+    $cache_key = PIXEL_CACHE_PREFIX . $calculated_signature . '_' . Utils::slugifyIp($_SERVER['REMOTE_ADDR']);
     $cached = $redis->get($cache_key);
     if ($cached === false && $calculated_signature === $_GET['key'] && preg_match("#\@justauth\.me$#", $_GET['from'])) {
-        $message = $_GET['to'] . ' opened an e-mail ' . date('Y-m-d \a\t H:i:s', Utils::time());
+        $message = $_GET['to'] . ' opened an e-mail ' . date('Y-m-d \a\t H:i:s', Utils::time()) . ' from ' . $_SERVER['REMOTE_ADDR'];
         $mailer = new Mailer();
         $mailer->queueMail(
             $_GET['from'],
