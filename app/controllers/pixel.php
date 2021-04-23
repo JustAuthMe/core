f0fb2e2260f33ed63ea73ddd2ea42ff154f70c11
phpitou <phpeter@phpeter.fr>
2021-04-23 16:40:05 +0200

    Show red pixel when error

--- app/controllers/pixel.php
+++ app/controllers/pixel.php
@@ -3,7 +3,9 @@ const PIXEL_SIGNING_KEY = 'aMJn6AbtUc855uL7BMdPwuMhRBU4WjuqNTtrHo58lGA2jz3jazn1Z
 const PIXEL_CACHE_PREFIX = 'pixel_';
 const PIXEL_CACHE_TTL = 86400; // 24 hours
 
+$base64 = 'iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mP8z8DwHwAFBQIAX8jx0gAAAABJRU5ErkJggg==';
 $not_allowed_ips = ['45.155.168.240', '185.216.24.254'];
+
 if (!in_array($_SERVER['REMOTE_ADDR'], $not_allowed_ips) && isset($_GET['from'], $_GET['to'], $_GET['time'], $_GET['key'])) {
     $calculated_signature = hash_hmac('sha256', $_GET['from'] . '.' . $_GET['to'] . '.' . $_GET['time'], PIXEL_SIGNING_KEY);
 
@@ -20,9 +22,10 @@ if (!in_array($_SERVER['REMOTE_ADDR'], $not_allowed_ips) && isset($_GET['from'],
             ['body' => $message]
         );
         $redis->set($cache_key, 1, PIXEL_CACHE_TTL);
+        $base64 = 'iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkYAAAAAYAAjCB0C8AAAAASUVORK5CYII=';
     }
 }
 
 header('Content-Type: image/png');
-echo base64_decode('iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkYAAAAAYAAjCB0C8AAAAASUVORK5CYII=');
+echo base64_decode($base64);
 die;
